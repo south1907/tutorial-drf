41df637bd818868e3ecf4d6e5926eda05224e426
Nam Pham <namph@hacvntech.com>
2024-10-04 10:36:39 +0700

    other way fix static

--- project/urls.py
+++ project/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls.static import static
+
 from django.contrib import admin
 from django.urls import path, include
 
@@ -24,5 +24,3 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
 ]
-
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
